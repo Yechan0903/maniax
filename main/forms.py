41839261d74c266211602db7cc9adfdbec1cd1d8
신예찬 <oolo9876@yonsei.ac.kr>
2024-08-04 18:56:01 +0900

    profilefront

--- main/forms.py
+++ main/forms.py
@@ -30,19 +30,19 @@ class ScreenTimeForm(forms.ModelForm):
 class CustomUserCreationForm(UserCreationForm):
     username = forms.CharField(
         label="아이디",
-        widget=forms.TextInput(attrs={'class': 'form-control'})
+        widget=forms.TextInput(attrs={'class': 'form-control','placeholder':"아이디"})
     )
     email = forms.EmailField(
         label="이메일",
-        widget=forms.EmailInput(attrs={'class': 'form-control'})
+        widget=forms.EmailInput(attrs={'class': 'form-control','placeholder':"이메일"})
     )
     password1 = forms.CharField(
         label="비밀번호",
-        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+        widget=forms.PasswordInput(attrs={'class': 'form-control','placeholder':"비밀번호"})
     )
     password2 = forms.CharField(
         label="비밀번호 재확인",
-        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+        widget=forms.PasswordInput(attrs={'class': 'form-control','placeholder':"비밀번호 재확인"})
     )
     class Meta:
         model = CustomUser
